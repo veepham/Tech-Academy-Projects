abfca5bf64a3da54b9a8980d8e197aaf69837094
veepham <111474183+veepham@users.noreply.github.com>
2022-09-01 16:38:18 -0700

    Live share

---
+++ Hello_World.js
@@ -0,0 +1,8 @@
+var greeting = 'Hello, world!';
+    console.log(greeting);
+
+var greeting2 = 'Welcome to Node.js';
+    console.log(greeting2);
+
+var sentence1 = 'Learning how to debug code witht the debugger';
+    console.log(sentence1);
